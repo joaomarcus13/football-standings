7c8ab91d61c2df8ce097fc4249ac952e7c624482
joaomarcus13 <joaomarcus13@hotmail.com>
2022-06-02 16:29:57 -0300

    add dotenv

--- server/config.js
+++ server/config.js
@@ -1,4 +1,5 @@
+import 'dotenv/config';
 export const apiconfig = {
   baseURL: 'http://api.football-data.org/v2/',
-  headers: { 'X-Auth-Token': '29302314f82f407cb9b903f87619a2a7' },
+  headers: { 'X-Auth-Token': process.env.TOKEN },
 };
